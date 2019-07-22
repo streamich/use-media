27d62c02ebc842b266c3643ea4344f32cfe3ec22
beefchimi <curtdulmage@gmail.com>
2019-07-22 09:13:09 -0400

    :art: [Code Quality] Refactor /src folder

---
+++ src/useMedia.ts
@@ -0,0 +1,53 @@
+import { useState, useEffect, useLayoutEffect } from "react";
+import { queryObjectToString, noop } from "./utilities";
+import { Effect, MediaQueryObject } from "./types";
+
+export const mockMediaQueryList: MediaQueryList = {
+  media: "",
+  matches: false,
+  onchange: noop,
+  addListener: noop,
+  removeListener: noop,
+  addEventListener: noop,
+  removeEventListener: noop,
+  dispatchEvent: (_: Event) => true
+};
+
+const createUseMedia = (effect: Effect) => (
+  rawQuery: string | MediaQueryObject,
+  defaultState = false
+) => {
+  const [state, setState] = useState(defaultState);
+  const query = queryObjectToString(rawQuery);
+
+  effect(() => {
+    let mounted = true;
+    const mediaQueryList: MediaQueryList =
+      typeof window === "undefined"
+        ? mockMediaQueryList
+        : window.matchMedia(query);
+
+    const onChange = () => {
+      if (!mounted) {
+        return;
+      }
+
+      setState(Boolean(mediaQueryList.matches));
+    };
+
+    mediaQueryList.addListener(onChange);
+    setState(mediaQueryList.matches);
+
+    return () => {
+      mounted = false;
+      mediaQueryList.removeListener(onChange);
+    };
+  }, [query]);
+
+  return state;
+};
+
+const useMedia = createUseMedia(useEffect);
+export const useMediaLayout = createUseMedia(useLayoutEffect);
+
+export default useMedia;
---
+++ src/utilities/index.ts
@@ -0,0 +1,3 @@
+export {default as camelToHyphen} from './camel-to-hypen';
+export {default as queryObjectToString} from './query-object-to-string';
+export {default as noop} from './noop';
